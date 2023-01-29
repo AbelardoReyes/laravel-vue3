fa10aa22f083cb6808b08c0bd70a73eb06d0ab46
Abelardo <abelardoreyes256@gmail.com>
2023-01-29 02:23:59 -0600

    Se termino la parte logica, pero falta el diseño

--- resources/js/Pages/Zapateria/Categoria/CategoriaFormularioCalzado.vue
+++ resources/js/Pages/Zapateria/Categoria/CategoriaFormularioCalzado.vue
@@ -73,8 +73,8 @@ function submit() {
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder="Existencia del calzado" />
                     <!--Error nombre-->
-                    <div v-if="errors.categoria" class="p-1 text-sm text-red-800 rounded-lg bg-red-200  dark:text-red-700"
-                        role="alert">
+                    <div v-if="errors.categoria"
+                        class="p-1 text-sm text-red-800 rounded-lg bg-red-200  dark:text-red-700" role="alert">
                         {{ errors.categoria }}
                     </div>
                     <input id="categoria" v-model="form.categoria"
