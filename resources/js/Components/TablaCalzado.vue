84614912608ea8de6c5a00dbf54c38c5776bfb09
Abelardo <abelardoreyes256@gmail.com>
2023-01-29 02:36:05 -0600

    Se agregaron iconos a las tablas

--- resources/js/Components/TablaCalzado.vue
+++ resources/js/Components/TablaCalzado.vue
@@ -22,8 +22,8 @@ defineProps({
                         <th class="pl-1 pr-1 border border-slate-600">Existencia</th>
                         <th class="pl-1 pr-1 border border-slate-600">Precio</th>
                         <th class="pl-1 pr-1 border border-slate-600">Categoria</th>
-                        <th class="pl-3 pr-3 border border-slate-600">Editar</th>
-                        <th class="pl-3 pr-3 border border-slate-600">Eliminar</th>
+                        <th class="pl-2 pr-2 border border-slate-600">Edit</th>
+                        <th class="pl-2 pr-2 border border-slate-600">Del</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -36,10 +36,26 @@ defineProps({
                         <td>{{ calzado.precio }}</td>
                         <td>{{ calzado.categoria }}</td>
                         <td>
-                            <Link :href="calzado.viewEdit" method="get" >Edit</Link>
+                            <Link :href="calzado.viewEdit" method="get">
+                            <svg class="h-7 w-7 text-teal-500" width="24" height="24" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M12 20h9" />
+                                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
+                            </svg>
+                            </Link>
                         </td>
                         <td>
-                            <Link :href="calzado.viewDelete" method="get" :data="{calzado:calzado}">Delete</Link>
+                            <Link :href="calzado.viewDelete" method="get" :data="{ calzado: calzado }"><svg
+                                class="h-7 w-7 text-red-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <line x1="4" y1="7" x2="20" y2="7" />
+                                <line x1="10" y1="11" x2="10" y2="17" />
+                                <line x1="14" y1="11" x2="14" y2="17" />
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                            </svg></Link>
                         </td>
                     </tr>
                 </tbody>
--- resources/js/Components/TablaCategorias.vue
+++ resources/js/Components/TablaCategorias.vue
@@ -18,8 +18,8 @@ defineProps({
                         <th class="pl-5 pr-5 border border-slate-600 ">Categoria</th>
                         <th class="pl-10 pr-10 border border-slate-600">Descripcion</th>
                         <th class="pl-5 pr-5 border border-slate-600">Nuevo Calzado</th>
-                        <th class="pl-5 pr-5 border border-slate-600">Editar</th>
-                        <th class="pl-5 pr-5 border border-slate-600">Eliminar</th>
+                        <th class="pl-5 pr-5 border border-slate-600">Edit</th>
+                        <th class="pl-5 pr-5 border border-slate-600">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,13 +27,40 @@ defineProps({
                         <td>{{ categoria.id }}</td>
                         <td>{{ categoria.nombre }}</td>
                         <td>{{ categoria.descripcion }}</td>
-                        <td><Link :href="categoria.viewCalzado" method="get" as="button">Nuevo</Link></td>
                         <td>
-                            <Link :href="categoria.viewEdit" method="get" as="button">Editar
+                            <Link :href="categoria.viewCalzado" method="get" as="button">
+                            <svg class="h-8 w-8 text-green-500" width="24" height="24" viewBox="0 0 24 24"
+                                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                                stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <line x1="9" y1="12" x2="15" y2="12" />
+                                <line x1="12" y1="9" x2="12" y2="15" />
+                                <path
+                                    d="M4 6v-1a1 1 0 0 1 1 -1h1m5 0h2m5 0h1a1 1 0 0 1 1 1v1m0 5v2m0 5v1a1 1 0 0 1 -1 1h-1m-5 0h-2m-5 0h-1a1 1 0 0 1 -1 -1v-1m0 -5v-2m0 -5" />
+                            </svg>
                             </Link>
                         </td>
                         <td>
-                            <Link :href="categoria.viewDelete" method="get" as="button">Eliminar
+                            <Link :href="categoria.viewEdit" method="get" as="button">
+                            <svg class="h-7 w-7 text-teal-500" width="24" height="24" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M12 20h9" />
+                                <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
+                            </svg>
+                            </Link>
+                        </td>
+                        <td>
+                            <Link :href="categoria.viewDelete" method="get" as="button"><svg
+                                class="h-7 w-7 text-red-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <line x1="4" y1="7" x2="20" y2="7" />
+                                <line x1="10" y1="11" x2="10" y2="17" />
+                                <line x1="14" y1="11" x2="14" y2="17" />
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                            </svg>
                             </Link>
                         </td>
                     </tr>
@@ -41,5 +68,5 @@ defineProps({
             </table>
         </div>
     </div>
-    
+
 </template>
